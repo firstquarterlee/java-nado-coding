92c646e1ec3b0fd135713c8f32d946928f793112
firstquarterlee <firstquarterlee@gmail.com>
2024-04-19 01:58:48 +0900

    Do While

---
+++ src/chap_04/_07_DoWhile.java
@@ -0,0 +1,41 @@
+package chap_04;
+
+public class _07_DoWhile {
+    public static void main(String[] args) {
+        // 반복문 DoWhile
+        int distance = 25; // 전체 거리 25m
+        int move = 0; // 현재 이동 거리 0m
+        int height = 2; // 키 2m
+        while (move + height < distance) { // move + heigth 이런것도됨
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        }
+        System.out.println("도착했습니다.");
+
+        System.out.println(" --- 반복문 #1 --- ");
+
+        // 키가 엄청나게 큰 사람?
+        move = 0;
+        height = 25; // 키가 25m
+        while (move + height < distance) {
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        }
+        System.out.println("도착했습니다.");
+
+        System.out.println(" --- 반복문 #2 --- ");
+
+        // Do While 반복문
+//        do {
+//          🤙여기 먼저 실행🤙
+//        } while (조건);
+        do {
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        } while (move + height < distance);
+        System.out.println("도착했습니다.");
+    }
+}
