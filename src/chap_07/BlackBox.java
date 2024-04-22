448184a927ea66b4a2b244d0f398992a12a32c08
firstquarterlee <firstquarterlee@gmail.com>
2024-04-23 07:34:06 +0900

    인스턴스 변수 선언

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -1,4 +1,8 @@
 package chap_07;
 
 public class BlackBox {
+    String modelName; // 모델명 -> 이렇게 4개의 값이 인스턴스 변수(또는 필드)라고한다.
+    String resolution; // 해상도
+    int price; // 가격
+    String color; // 색상
 }
